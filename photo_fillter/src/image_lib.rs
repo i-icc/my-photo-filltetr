c749e5f948eb3419cc6c5832cb44189ee0942a04
i-icc <ichi623110@gmail.com>
2024-10-13 19:16:16 +0900

    add split

--- photo_fillter/src/image_lib.rs
+++ photo_fillter/src/image_lib.rs
@@ -1,4 +1,4 @@
-use image::RgbaImage;
+use image::{GenericImageView, ImageBuffer, RgbaImage};
 
 pub fn grayscale_fillter(img_data: &[u8], width: u32, height: u32) -> Vec<u8> {
     let img = RgbaImage::from_raw(width, height, img_data.to_vec()).unwrap();
@@ -19,4 +19,27 @@ pub fn grayscale_fillter(img_data: &[u8], width: u32, height: u32) -> Vec<u8> {
     }
 
     rgba_data
+}
+
+pub fn split_image(img_data: &[u8], width: u32, height: u32) -> [RgbaImage; 4] {
+    // 画像全体のRGBAピクセルのバッファを読み込み
+    let img =
+        ImageBuffer::from_raw(width, height, img_data.to_vec()).expect("Failed to create image");
+
+    // 各部分の幅と高さを計算（4分割）
+    let half_width = width / 2;
+    let half_height = height / 2;
+
+    // 左上
+    let top_left = img.view(0, 0, half_width, half_height).to_image();
+    // 右上
+    let top_right = img.view(half_width, 0, half_width, half_height).to_image();
+    // 左下
+    let bottom_left = img.view(0, half_height, half_width, half_height).to_image();
+    // 右下
+    let bottom_right = img
+        .view(half_width, half_height, half_width, half_height)
+        .to_image();
+
+    [top_left, top_right, bottom_left, bottom_right]
 }
--- photo_fillter/src/main.rs
+++ photo_fillter/src/main.rs
@@ -1,7 +1,7 @@
 mod image_lib;
 
-use crate::image_lib::grayscale_fillter;
 use image::{ImageBuffer, Rgba};
+use image_lib::{grayscale_fillter, split_image};
 use std::fs::File;
 use std::io::BufReader;
 
@@ -9,17 +9,9 @@ fn main() {
     // Define input and output paths
     const FILE_PATH: &str = "./test-images/logo.png";
     const GRAY_TEST_PATH: &str = "./test-images/grayscale_test.png";
+    const SPLIT_TEST_PATH: &str = "./test-images/split_";
 
-    // Run the grayscale test
-    if let Err(e) = grayscale_test(FILE_PATH, GRAY_TEST_PATH) {
-        eprintln!("Error processing image: {}", e);
-    }
-}
-
-// Grayscale conversion and saving to output file
-fn grayscale_test(file_path: &str, output_path: &str) -> Result<(), Box<dyn std::error::Error>> {
-    // Open image file
-    let file = File::open(file_path).expect("Failed to open input image file");
+    let file = File::open(FILE_PATH).expect("Failed to open input image file");
     let reader = BufReader::new(file);
 
     // Load and convert the image to RGBA8
@@ -30,6 +22,37 @@ fn grayscale_test(file_path: &str, output_path: &str) -> Result<(), Box<dyn std:
     // Get image dimensions
     let (width, height) = img.dimensions();
 
+    // Run the grayscale test
+    if let Err(e) = grayscale_test(&img, width, height, GRAY_TEST_PATH) {
+        eprintln!("Error processing image: {}", e);
+    }
+
+    // split test
+    if let Err(e) = split_test(&img, width, height, SPLIT_TEST_PATH) {
+        eprintln!("Error processing image: {}", e);
+    }
+}
+
+fn split_test(
+    img: &[u8],
+    width: u32,
+    height: u32,
+    output_path: &str,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let split_images = split_image(&img, width, height);
+    for (i, img) in split_images.iter().enumerate() {
+        img.save(format!("{}{}_test.png", output_path, i))
+            .expect("Failed to save image");
+    }
+    Ok(())
+}
+
+fn grayscale_test(
+    img: &[u8],
+    width: u32,
+    height: u32,
+    output_path: &str,
+) -> Result<(), Box<dyn std::error::Error>> {
     // Apply grayscale transformation
     let img_data = grayscale_fillter(&img, width, height);
 
